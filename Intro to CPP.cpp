160e5b196cda96d2f4bb334ebec41184d178c332
GS99 <gurvindersingh16199@gmail.com>
2024-09-14 01:58:09 +0530

    Start with Basics of c++

---
+++ Intro to CPP.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    #pragma intro to C++
+    // cout<<"Hello world"<<endl;
+    // cout<<"Hello Gurvinder"<<endl;
+
+    // float a=5/2.0;
+    // cout<<"a="<<a<<" and size of a= "<<sizeof(a)<<endl;
+    // char b=65;
+    // cout<<b<<endl;
+
+    // bool c=0;
+    // cout<<"c = "<<c<<endl;
+#pragma endregion;
+
+#pragma Conditions
+    //Check if num is +ve, -ve or 0
+        // int num;
+        // cout<<"Enter num:";
+        // cin>>num;
+        // if(num>0)
+        // cout<<"num is +ve";
+        // else if(num<0){
+        //     cout<<"num is -ve";
+        // }
+        // else{
+        //     cout<<"num is 0";             //optional else
+        // } 
+
+    // Input: ch
+    //Output: 
+    //if ch between 'a' to 'z' print Lowercase
+    //if ch between 'A' to 'Z' print Uppercase
+    //if ch between '1' to '9' print Numeric
+
+        // char ch;
+        // cout << "Enter chracter:";
+        // cin >> ch;
+        // if (ch >= 'a' && ch <= 'z')
+        //     cout << "Lowercase";
+        // else if (ch >= 'A' && ch <= 'Z')
+        // {
+        //     cout << "Uppercase";
+        //     cout << ch;
+        // }
+        // else if (ch >= '0' && ch <= '9')
+        //     cout << "Numeric";
+
+#pragma endregion
+
+#pragma Loops
+    //Print sum from 1 to n
+        // int n;
+        // cout<<"Enter n";
+        // cin>>n;
+        // int i=1,sum=0;
+        // while(i<=n){
+        //     sum+=i;
+        //     i++;
+        // }
+        // cout<<"Sum is "<<sum;
+
+    //Print sum of even num till n;
+        // int n;
+        // cout<<"Enter n";
+        // cin>>n;
+        // int i=2,sum=0;
+        // while(i<=n){
+        //     sum+=i;
+        //     i+=2;
+        // }
+        // cout<<"Sum of even num is "<<sum;
+
+    // Print table of farenheight to celcius
+        // int n;
+        // cout<<"Enter n ";
+        // cin>>n;
+        // int f=1;
+        // float c;
+        // while(f<=n){
+        //     c=(f-32)*5/9.0;
+        //     cout<<"F : "<<f<<" C : "<<c<<endl;
+        //     f++;
+        // }
+
+    // Check if num is prime or not
+        // int n;
+        // cout<<"Enter n : ";
+        // cin>>n;
+        // int i=2;
+        // bool isPrime=1;
+        // while(i<n){
+        //     if(n%i==0)
+        //         isPrime=0;
+        //     i++;
+        // }
+        // isPrime?cout<<"Prime":cout<<"Not Prime";
+
+#pragma endregion
+}
