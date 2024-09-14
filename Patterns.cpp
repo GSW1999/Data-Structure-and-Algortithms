616c9fb1ed873186daeeb3f7f29b1ad69f41d388
GS99 <gurvindersingh16199@gmail.com>
2024-09-14 23:51:38 +0530

    Basics of patterns printing

---
+++ Patterns.cpp
@@ -0,0 +1,283 @@
+#include<iostream>
+using namespace std;
+int main(){
+/* 
+// Print Pattern
+
+// ****      **
+// ****      **
+// ****      
+
+    int i=1,j,n;
+    cout<<"Enter n : ";
+    cin>>n;
+    while(i<=n){
+        j=1;
+        while(j<=n){
+            cout<<"*";
+            j+=1;
+        }
+        cout<<endl;
+        i+=1;
+    }
+*/
+
+ /*
+ // Print Pattern
+    
+// 111      1111
+// 222      2222
+// 333      3333
+//          4444
+
+    int i=1,j,n;
+    cout<<"Enter n : ";
+    cin>>n;
+    while(i<=n){
+        j=1;
+        while(j<=n){
+            cout<<i;
+            j+=1;
+        }
+        cout<<endl;
+        i+=1;
+    }
+*/
+
+/*
+// Print Pattern
+
+// 123      1234
+// 123      1234
+// 123      1234
+//          1234
+
+    int i=1,j,n;
+    cout<<"Enter n : ";
+    cin>>n;
+    while(i<=n){
+        j=1;
+        while(j<=n){
+            cout<<j;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+*/
+
+/*
+// Print Pattern
+  
+// 321      4321
+// 321      4321
+// 321      4321
+//          4321
+
+    // int i=1,j,n;
+    // cout<<"Enter n : ";
+    // cin>>n;
+    // while(i<=n){
+    //     j=4;
+    //  while(j>=1){
+    //     cout<<j;
+    //     j--;
+    //  }
+    //     i++;
+    //     cout<<endl;
+    // }
+
+    //or
+
+    int i=1,j,n;
+    cout<<"Enter n : ";
+    cin>>n;
+    while(i<=n){
+        j=1;
+    while(j<=n){
+        cout<<n-j+1;
+        j++;
+    }
+        i++;
+        cout<<endl;
+    }
+*/
+
+/*
+// Print Pattern
+    
+// 123      
+// 456      
+// 789              
+
+    int i=1,j=1,counter=1,n;
+    cout<<"Enter n : ";
+    cin>>n;
+    while(i<=n){
+        j=1;
+        while(j<=n){
+            cout<<counter;
+            counter++;
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+*/
+
+/*
+// Print Pattern
+   
+// *     
+// **      
+// ***      
+// ****         
+
+    int i=1,j,n;
+    cout<<"Enter n : ";
+    cin>>n;
+    while(i<=n){
+    j=1;
+    while(j<=i)
+    {
+        cout<<"*";
+        j++;
+    }
+        cout<<endl;
+        i++;
+    }
+*/
+
+/*
+// Print Pattern
+    
+// 1     
+// 22      
+// 333      
+// 4444
+
+    int i = 1, j, n;
+    cout << "Enter n : ";
+    cin >> n;
+    while (i <= n)
+    {
+
+        j = 1;
+        while (j <= i)
+        {
+            cout << i;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+    */
+
+/*
+// Print Pattern
+    
+// ****     
+// ***      
+// **      
+// *
+   int i = 1, j, n;
+    cout << "Enter n : ";
+    cin >> n;
+    while (i <= n)
+    {
+        j = 1;
+        while (j <= n-i+1)
+        {
+            cout << "*";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+*/
+
+/*
+// Print Pattern
+   
+// 1  
+// 23      
+// 345      
+// 4567
+
+    int i = 1, j,val, n;
+    cout << "Enter n : ";
+    cin >> n;
+    while (i <= n)
+    {
+        //with variable
+            // j=1;
+            // val=i;
+            // while (j <=i)
+            // {
+            //     cout << val;
+            //     val++;
+            //     j++;
+            // }
+            // cout << endl;
+            // i++;
+
+        //Without varible
+            // j=1;
+            // while(j<=i){
+            //     cout<<i+j-1;
+            //     j++;
+            // }
+            // cout<<endl;
+            // i++;
+
+            //or
+
+            // j=0;
+            // while(j<i){
+            //     cout<<i+j;
+            //     j++;
+            // }
+            // cout<<endl;
+            // i++;
+
+    }
+*/
+
+/*
+// Print Pattern
+ 
+// 1  
+// 21      
+// 321      
+// 4567
+
+    int i = 1, j,val, n;
+    cout << "Enter n : ";
+    cin >> n;
+    while (i <= n){
+    //with variable
+        //     val=i;
+        //     j=1;
+        //     while(j<=i){
+        //         cout<<val;
+        //         val--;
+        //         j++;
+        //     }
+        //     cout<<endl;
+        //     i++;
+        
+
+    //Witout variable
+    j=1;
+    while(j<=i){
+        cout<<i-j+1;
+        j++;
+    }
+    cout<<endl;
+    i++;
+   }
+
+*/
+
+  /* */
+}
